37c81f44843e263c17a11c22aa19dffffdacf668
love <kshitizluv123@gmail.com>
2026-08-27 23:53:53 +0545

    transform function to clean the raw data

---
+++ Daraz/transform.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def transform_product_data(product_name, product_price, product_rating, product_count):
+    # Create a DataFrame from the extracted product data
+    data = {
+        'Product Name': [product_name],
+        'Product Price': [product_price],
+        'Product Rating': [product_rating],
+        'Product Count': [product_count]
+    }
+    df = pd.DataFrame(data)
+
+    # Perform any necessary transformations (e.g., cleaning, formatting)
+    # Clean the price field if it contains currency text
+    if df["Product Price"].iloc[0] != "N/A":
+        df["Product Price"] = (
+            df["Product Price"]
+            .str.replace("Rs. ", "", regex=False)
+            .str.replace(",", "")
+            .astype(float)
+        )
+    return df
